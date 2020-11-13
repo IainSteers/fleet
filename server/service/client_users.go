c4b3f5a9e9f428775b34f2eec60f504e818cd7b9
Iain Steers <isteers@palantir.com>
2020-11-13 13:01:14 +0000

    List/GetUser client support

--- server/service/client_users.go
+++ server/service/client_users.go
@@ -2,10 +2,10 @@ package service
 
 import (
 	"encoding/json"
-	"net/http"
-
+	"fmt"
 	"github.com/fleetdm/fleet/server/kolide"
 	"github.com/pkg/errors"
+	"net/http"
 )
 
 // CreateUser creates a new user, skipping the invitation process.
@@ -36,4 +36,67 @@ func (c *Client) CreateUser(p kolide.UserPayload) error {
 	}
 
 	return nil
+}
+
+// GetUser retrieves information about a user
+func (c *Client) GetUser(id uint) (*kolide.User, error) {
+	verb, path := "GET", fmt.Sprintf("/api/v1/kolide/users/%d", id)
+	response, err := c.AuthenticatedDo(verb, path, "", nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "GET /api/v1/kolide/users")
+	}
+	defer response.Body.Close()
+
+	switch response.StatusCode {
+	case http.StatusNotFound:
+		return nil, notFoundErr{}
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.Errorf(
+			"get user received status %d %s",
+			response.StatusCode,
+			extractServerErrorText(response.Body),
+		)
+	}
+
+	var responseBody getUserResponse
+	err = json.NewDecoder(response.Body).Decode(&responseBody)
+	if err != nil {
+		return nil, errors.Wrap(err, "decode get user response")
+	}
+
+	if responseBody.Err != nil {
+		return nil, errors.Errorf("get user: %s", responseBody.Err)
+	}
+
+	return responseBody.User, nil
+}
+
+// ListUsers retrieves the list of all Users.
+func (c *Client) ListUsers() ([]kolide.User, error) {
+	response, err := c.AuthenticatedDo("GET", "/api/v1/kolide/users", "", nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "GET /api/v1/kolide/users")
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.Errorf(
+			"list users received status %d %s",
+			response.StatusCode,
+			extractServerErrorText(response.Body),
+		)
+	}
+
+	var responseBody listUsersResponse
+	err = json.NewDecoder(response.Body).Decode(&responseBody)
+	if err != nil {
+		return nil, errors.Wrap(err, "decode list users response")
+	}
+
+	if responseBody.Err != nil {
+		return nil, errors.Errorf("list users: %s", responseBody.Err)
+	}
+
+	return responseBody.Users, nil
 }
